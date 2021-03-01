118a0eee0188cadd651ece5ac5aa9d241a18f99e
Wesley Salimans <me@wesleysalimans.com>
2021-03-01 11:34:27 +0100

    added route authorization

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::group(['middleware' => ['role:admin']], function () {
+    Route::get('/home', function () {
+        return view('home');
+    });
 });
