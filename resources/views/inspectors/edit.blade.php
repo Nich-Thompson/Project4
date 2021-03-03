00ccefde8a9688b7871c53d61023c4dc6f724de0
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-03-03 17:37:30 +0100

    Added JS

--- resources/views/inspectors/edit.blade.php
+++ resources/views/inspectors/edit.blade.php
@@ -2,7 +2,19 @@
 
 @section('content')
     <div class="py-12 px-4">
-        <h1>ye</h1>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-12 text-left">
+                <h1>
+                    Inspecteur bewerken
+                </h1>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-12 text-right">
+                <h1>
+                    <button id="archiveButton" class="btn btn-primary" hidden>Archiveren</button>
+                    <button id="switchButton" onclick="enableInput()" class="btn btn-primary">Bewerken</button>
+                </h1>
+            </div>
+        </div>
 
         @if ($errors->any())
             <div class="alert alert-danger">
@@ -22,32 +34,32 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <input type="text" name="first_name" value="{{ $user->first_name }}" class="form-control">
+                        <input type="text" name="first_name" value="{{ $user->first_name }}" class="form-control" disabled>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <input type="text" name="last_name" value="{{ $user->last_name }}" class="form-control">
+                        <input type="text" name="last_name" value="{{ $user->last_name }}" class="form-control" disabled>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <input type="text" name="email" value="{{ $user->email }}" class="form-control">
+                        <input type="text" name="email" value="{{ $user->email }}" class="form-control" disabled>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <input type="text" name="phone_number" value="{{ $user->phone_number }}" class="form-control">
+                        <input type="text" name="phone_number" value="{{ $user->phone_number }}" class="form-control" disabled>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <input type="text" name="password" value="" class="form-control" placeholder="Wachtwoord*">
+                        <input type="text" name="password" value="" class="form-control" placeholder="Wachtwoord*" disabled>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <input type="text" name="password_confirmation" value="" class="form-control" placeholder="Herhaal wachtwoord*">
+                        <input type="text" name="password_confirmation" value="" class="form-control" placeholder="Herhaal wachtwoord*" disabled>
                     </div>
                 </div>
 
@@ -55,13 +67,15 @@
                     Door de wachtwoorden leeg te laten worden deze niet veranderd.
                 </div>
 
-                <div class="col-xs-12 col-sm-12 col-md-12 text-leftf">
+                <div class="col-xs-12 col-sm-12 col-md-6 text-left">
                     <a href="{{ url()->previous() }}" class="btn btn-default">Terug</a>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 text-right">
+                <div class="col-xs-12 col-sm-12 col-md-6 text-right">
                     <button type="submit" class="btn btn-primary">Opslaan</button>
                 </div>
             </div>
         </form>
     </div>
 @endsection
+
+<script src="{{ asset('js/switchEditView.js') }}"></script>
