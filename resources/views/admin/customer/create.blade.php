d05f5e5de26cdda09926e60f2a920c220009a8b6
DaanMonk <d.groot3@student.avans.nl>
2021-03-04 10:02:00 +0100

    Modified 2 pages

--- resources/views/admin/customer/create.blade.php
+++ resources/views/admin/customer/create.blade.php
@@ -2,61 +2,54 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
                     <span class="float-left h2">Create new customer</span>
-                    <a href="{{URL::to('/customer')}}" class="float-right btn btn-primary">Back</a>
+                    <a href="{{URL::to('/customer')}}" class="w-25 btn btn-primary">Back</a>
                     <br><br>
                     <form action="{{ route('postCustomerCreate') }}" method="post">
                         @csrf
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" class="form-control" id="name" name="name"
-                                   placeholder="Enter customer name">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter customer name">
                         </div>
                         <div class="form-group">
                             <label for="city">City</label>
-                            <input type="text" class="form-control" id="city" name="city"
-                                   placeholder="Enter city name">
+                            <input type="text" class="form-control" id="city" name="city" placeholder="Enter city name">
                         </div>
                         <div class="form-group">
                             <label for="street">Street</label>
-                            <input type="text" class="form-control" id="street" name="street"
-                                   placeholder="Enter street name">
+                            <input type="text" class="form-control" id="street" name="street" placeholder="Enter street name">
                         </div>
                         <div class="form-group">
                             <label for="number">House number</label>
-                            <input type="number" class="form-control" id="number" name="number"
-                            placeholder="Enter house number">
+                            <input type="number" class="form-control" id="number" name="number" placeholder="Enter house number">
                         </div>
                         <div class="form-group">
                             <label for="postal_code">Postal code</label>
-                            <input type="text" class="form-control" id="postal_code" name="postal_code"
-                                   placeholder="1234AA">
+                            <input type="text" class="form-control" id="postal_code" name="postal_code" placeholder="1234AA">
                         </div>
                         <div class="form-group">
                             <label for="contact_name">Contact name</label>
-                            <input type="text" class="form-control" id="contact_name" name="contact_name"
-                                   placeholder="Enter contact name">
+                            <input type="text" class="form-control" id="contact_name" name="contact_name" placeholder="Enter contact name">
                         </div>
                         <div class="form-group">
                             <label for="contact_phone_number">Contact phone number</label>
-                            <input type="text" class="form-control" id="contact_phone_number" name="contact_phone_number"
-                                   placeholder="06 12345678">
+                            <input type="text" class="form-control" id="contact_phone_number" name="contact_phone_number" placeholder="06 12345678">
                         </div>
                         <div class="form-group">
                             <label for="contact_email">Contact email</label>
-                            <input type="text" class="form-control" id="contact_email" name="contact_email"
-                                   placeholder="name@provider.com">
+                            <input type="text" class="form-control" id="contact_email" name="contact_email" placeholder="name@provider.com">
                         </div>
                         <br>
                         <button type="submit" class="btn btn-success">Create</button>
                     </form>
+                    <div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
-
+        @endsection
--- resources/views/admin/customer/index.blade.php
+++ resources/views/admin/customer/index.blade.php
@@ -2,14 +2,35 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card text-center">
+                <div class="card-body">
                     <span class="float-left h2">Customers</span>
-
+                    <a href="customer/create">
+                    <button class="btn bg-success">Aanmaken</button>
+                    </a>
+                </div>
+                <div class="mx-4">
+                    <table class="table">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <td>ExampleID</td>
+                            <td>ExampleName</td>
+                            <td>
+                            <input type="submit" class="btn sahdow-sm mr-4" value="Selecteer" />
+                            <input type="submit" class="btn sahdow-sm" value="Aanpassen" />
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
