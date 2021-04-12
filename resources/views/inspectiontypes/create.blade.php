e251a02377c66ac79658dbcff5791ec8290c5733
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-04-12 15:05:25 +0200

    Update create.blade.php

--- resources/views/inspectiontypes/create.blade.php
+++ resources/views/inspectiontypes/create.blade.php
@@ -14,26 +14,29 @@
                 </ul>
             </div>
         @endif
-        TODO STYLING
         <form action="{{ route('postInspectionTypeCreate') }}" method="POST">
             @csrf
             <div class="row">
                 <div class="col-xs-6 col-sm-6 col-md-6">
+                    <label>Naam van het insspectietype:</label>
                     <div class="form-group">
-                        <input type="text" name="name" class="form-control" value = "{{old('name')}}" placeholder="Naam type*" required oninvalid="this.setCustomValidity('Dit veld mag niet leeg zijn.')" oninput="this.setCustomValidity('')">
+                        <input type="text" name="name" class="form-control" value = "{{old('name')}}" placeholder="Naam type" required oninvalid="this.setCustomValidity('Dit veld mag niet leeg zijn.')" oninput="this.setCustomValidity('')">
                     </div>
                 </div>
                 <div class="col-xs-6 col-sm-6 col-md-6">
+                    <label>Beschrijving:</label>
                     <div class="form-group">
-                        <input type="text" name="description" class="form-control" value = "{{old('description')}}" placeholder="Beschrijving*" required oninvalid="this.setCustomValidity('Dit veld mag niet leeg zijn.')" oninput="this.setCustomValidity('')">
+                        <input type="text" name="description" class="form-control" value = "{{old('description')}}" placeholder="Beschrijving" required oninvalid="this.setCustomValidity('Dit veld mag niet leeg zijn.')" oninput="this.setCustomValidity('')">
                     </div>
                 </div>
                 <div class="col-xs-6 col-sm-6 col-md-6">
+                    <label>Kleur:</label>
                     <div class="form-group">
                         <input type="color" name="color" class="form-control" value = "{{old('color')}}" required oninvalid="this.setCustomValidity('Dit veld mag niet leeg zijn.')" oninput="this.setCustomValidity('')">
                     </div>
                 </div>
                 <div class="col-xs-6 col-sm-6 col-md-6">
+                    <label>Icoon:</label>
                     <div class="form-group">
                         <select name="icon_id" class="col-xs-12 col-sm-12 col-md-12 form-control">
                             @foreach($icons as $icon)
@@ -43,10 +46,6 @@
                             @endforeach
                         </select>
                     </div>
-                </div>
-
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    Velden met een ster(*) zijn verplicht.
                 </div>
 
                 <div class="col-xs-12 col-sm-12 col-md-12 text-leftf">
