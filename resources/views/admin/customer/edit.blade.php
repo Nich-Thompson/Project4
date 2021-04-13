99eb767ec950e5d302aaad668e37fdec741f206f
karsten <karsten.belt01@gmail.com>
2021-04-13 12:33:27 +0200

    Update edit.blade.php

--- resources/views/admin/customer/edit.blade.php
+++ resources/views/admin/customer/edit.blade.php
@@ -169,7 +169,7 @@
                                                         <a id="{{$location->id}}" href="{{route('getLocationEdit', [$customer->id, $location->id]) }}" class="btn btn-primary">
                                                             Bewerken
                                                         </a>
-                                                        <a id="{{$location->id}}" href="{{URL::to('/inspection/create')}}" class="btn border float-right ml-2 mt-2">
+                                                        <a id="{{$location->id}}" href="{{URL::to('/inspection/create')}}" class="btn border float-right ml-2">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
                                                             </svg>Maak inspectie aan</a>
