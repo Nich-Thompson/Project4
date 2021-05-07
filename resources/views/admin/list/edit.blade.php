f592d46f6b7bbb4750e42875b25a04079488d1c2
milovdpas <mpmpas1@avans.nl>
2021-05-07 14:09:06 +0200

    fixed styling and labels

--- resources/views/admin/list/edit.blade.php
+++ resources/views/admin/list/edit.blade.php
@@ -19,7 +19,7 @@
                                 d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                         </svg>Toevoegen</a>
                     <p class="mb-5"></p>
-                    <hr/>
+                    <hr>
 
 {{--                    <label class="ml-1">Lijstnaam:</label>--}}
                     <div class="d-flex flex-column">
