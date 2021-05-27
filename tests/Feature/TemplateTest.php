7c91af9880d73dda0145abfca2b34ff8c21e8559
milovdpas <mpmpas1@avans.nl>
2021-05-27 14:14:20 +0200

    written test

--- tests/Feature/TemplateTest.php
+++ tests/Feature/TemplateTest.php
@@ -40,17 +40,25 @@ class TemplateTest extends TestCase
 
         $this->be($user);
 
-        Icon::create([
+        $icon = Icon::create([
             'name' => 'fire-extinguisher',
             'unicode' => 'f134'
         ]);
-        InspectionType::create([
-            'icon_id' => 1,
+        $this->inspection_type = InspectionType::create([
+            'icon_id' => $icon->id,
             'name' => 'Brandblussers',
             'description' => 'Alle brandblussers moeten niet over de datum zijn.',
             'color' => '#FF3333'
         ]);
     }
+
+    public function test_index_screen_can_be_rendered()
+    {
+        $response = $this->get('/template');
+
+        $response->assertStatus(200);
+    }
+
 
     public function test_create_screen_can_be_rendered()
     {
@@ -62,121 +70,18 @@ class TemplateTest extends TestCase
     public function test_new_template_can_be_created()
     {
         Template::create([
-            'inspection_type_id' => 1,
+            'inspection_type_id' => $this->inspection_type->id,
             'json' => '[]',
         ]);
         $templates = Template::all();
         $oldTemplate = $templates[count($templates)-1];
         $controller = new TemplateController();
         $controller->store(new Request([
-            'type_id' => 1
+            'type_id' => $this->inspection_type->id
         ]));
 
         $templates = Template::all();
         $newTemplate = $templates[count($templates)-1];
         $this->assertFalse($oldTemplate->is($newTemplate));
-    }
-
-    public function test_edit_screen_can_be_rendered()
-    {
-        $list = ListModel::create([
-            'name' => 'test',
-            'list_model_id' => null
-        ]);
-        $response = $this->get('/list/'.$list->id.'/edit');
-
-        $response->assertStatus(200);
-    }
-
-    public function test_list_can_be_edited()
-    {
-        $oldList = ListModel::create([
-            'name' => 'test_list',
-            'list_model_id' => null
-        ]);
-
-        $controller = new ListController();
-        $controller->update($oldList->id, new ListRequest([
-            'name' => 'test_list_1'
-        ]));
-
-        $newList = ListModel::find($oldList->id);
-
-        $this->assertFalse($oldList->name === $newList->name);
-    }
-
-    public function test_createValue_screen_can_be_rendered()
-    {
-        $list = ListModel::create([
-            'name' => 'test',
-            'list_model_id' => null
-        ]);
-        $response = $this->get('/list/'.$list->id.'/create-value');
-
-        $response->assertStatus(200);
-    }
-
-    public function test_new_listValue_can_be_created()
-    {
-        $list = ListModel::create([
-            'name' => 'test',
-            'list_model_id' => null
-        ]);
-        $listValue = ListValue::create([
-            'name' => 'test',
-            'list_model_id' => $list->id,
-            'list_value_id' => null
-        ]);
-        $listValues = ListValue::all();
-        $oldValue = $listValues[count($listValues)-1];
-        $controller = new ListController();
-        $controller->storeValue(new StoreListValueRequest([
-            'name' => 'test_value_2',
-            'sublist_value' => null
-        ]), $list->id);
-
-        $listValues = ListValue::all();
-        $newValue = $listValues[count($listValues)-1];
-        $this->assertFalse($oldValue->is($newValue));
-    }
-
-    public function test_editValue_screen_can_be_rendered()
-    {
-        $list = ListModel::create([
-            'name' => 'test',
-            'list_model_id' => null
-        ]);
-        $listValue = ListValue::create([
-            'name' => 'test',
-            'list_model_id' => $list->id,
-            'list_value_id' => null
-        ]);
-        $response = $this->get('/list/'.$list->id.'/'.$listValue->id.'/edit-value');
-
-        $response->assertStatus(200);
-    }
-
-    public function test_listValue_can_be_edited()
-    {
-        $list = ListModel::create([
-            'name' => 'test',
-            'list_model_id' => null
-        ]);
-        $oldValue = ListValue::create([
-            'name' => 'test_value_2',
-            'list_model_id' => $list->id,
-            'list_value_id' => null
-        ]);
-
-        $controller = new ListController();
-        $controller->updateValue(new StoreListValueRequest([
-            'name' => 'test_list',
-            'sublist_value' => null
-        ]),1, $oldValue->id);
-
-        $newValue = ListValue::find($oldValue->id);
-
-        $this->assertFalse($oldValue->name === $newValue->name);
-        $this->assertTrue($oldValue->list_value_id === $newValue->list_value_id);
     }
 }
