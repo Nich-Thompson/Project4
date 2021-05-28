a2daf4e170a1b6547ef6a6ef5935080792f9f8f9
milovdpas <mpmpas1@avans.nl>
2021-05-28 13:08:05 +0200

    added icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
           crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/x-icon" href="{{url('images/inspectietool.png')}}" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
