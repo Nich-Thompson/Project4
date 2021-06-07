955af72e281306fc8a62a8810c3df83fcb0d5dac
milovdpas <mpmpas1@avans.nl>
2021-06-07 13:00:40 +0200

    every inspection line is now ordered

--- public/js/inspection.js
+++ public/js/inspection.js
@@ -271,10 +271,33 @@ function saveNewObject(object) {
     if (!(storage instanceof Array))
         storage = [storage];
     storage.push(object);
+    storage.sort(alphaNumericSort);
 
     localStorage.setItem("inspections", JSON.stringify(storage));
 
     syncData();
+}
+
+function alphaNumericSort(as, bs){
+    as = as[1].value;
+    bs = bs[1].value;
+    let a, b, a1, b1, i= 0, n, L,
+        rx=/(\.\d+)|(\d+(\.\d+)?)|([^\d.]+)|(\.\D+)|(\.$)/g;
+    if(as=== bs) return 0;
+    a= as.toLowerCase().match(rx);
+    b= bs.toLowerCase().match(rx);
+    L= a.length;
+    while(i<L){
+        if(!b[i]) return 1;
+        a1= a[i],
+            b1= b[i++];
+        if(a1!== b1){
+            n= a1-b1;
+            if(!isNaN(n)) return n;
+            return a1>b1? 1:-1;
+        }
+    }
+    return b[i]? -1:0;
 }
 
 function syncData() {
